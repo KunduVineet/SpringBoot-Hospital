3ee39551674f2fba1d7ef6fa576eb0acf4f42b62
VineetKundu <kunduvineet6@gmail.com>
2025-05-02 15:04:46 +0530

    Project Setup

---
+++ Hospital-SpringBoot-Project/src/main/java/com/hospital/Hospital/SpringBoot/Project/HospitalSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.Hospital.SpringBoot.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalSpringBootProjectApplication.class, args);
+	}
+
+}
---
+++ Hospital-SpringBoot-Project/src/test/java/com/hospital/Hospital/SpringBoot/Project/HospitalSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.Hospital.SpringBoot.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
