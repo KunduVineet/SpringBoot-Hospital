8eedeca4c46a2368e3b4f394285dfe73c4240817
VineetKundu <kunduvineet6@gmail.com>
2025-05-04 15:54:40 +0530

    Repository Created

---
+++ Hospital-SpringBoot-Project/src/main/java/Repository/AppointmentRepository.java
@@ -0,0 +1,7 @@
+package Repository;
+
+import Model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
+}
---
+++ Hospital-SpringBoot-Project/src/main/java/Repository/DoctorRepository.java
@@ -0,0 +1,8 @@
+package Repository;
+
+import Model.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
+
+}
